1f785e725e1ba6794a60333cdb24fef728c43b58
Python Script <script@example.com>
2025-07-02 18:05:38 +0700

    Cập nhật index.php lúc 2025-07-02 18:05:37.878405

--- index.php
+++ index.php
@@ -28,4 +28,7 @@ $content = "<h1 style='color: green;'>Trang web đã hoạt động!</h1>
 <body>
     <?= $content ?>
 </body>
-</html>
+</html>#EXTINF:-1 ,TRỰC TIẾP
+#EXTVLCOPT:http-user-agent=Dalvik/2.1.0
+https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1751475932/ei/fBJlaKzKLMaU1d8P6_6h-AQ/ip/116.97.99.121/id/7VjL3Cs9LXw.1/itag/301/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D299/rqh/1/hls_chunk_host/rr2---sn-8pxuuxa-i5osz.googlevideo.com/xpc/EgVo2aDSNQ%3D%3D/playlist_duration/30/manifest_duration/30/bui/AY1jyLMlXZesm1qKwpD7Zfm8koZv81QBzXg7D6batngjQHAq5Mvj8c-lR8pJRFNOqWG8QKvtfGrH1ATZ/spc/l3OVKfVdRj8CdlnyqoC9voaDKpxUrxQ4qCSJ3xguWnv1AprS10-7pS_KjKWri12RsYVfReR-7Cg/vprv/1/playlist_type/DVR/initcwndbps/3277500/met/1751454334,/mh/DR/mm/44/mn/sn-8pxuuxa-i5osz/ms/lva/mv/m/mvi/2/pl/22/rms/lva,lva/dover/11/pacing/0/keepalive/yes/fexp/51355912/mt/1751453790/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,rqh,xpc,playlist_duration,manifest_duration,bui,spc,vprv,playlist_type/sig/AJfQdSswRgIhANZZ7gD_EAv9wsciKBp1Zd0yTsR98jBvwEm0xME3WsIxAiEA9U3L1BhdtHccQDPoDdOdiQpB2QeniZ5azd_5tE7DXgY%3D/lsparams/hls_chunk_host,initcwndbps,met,mh,mm,mn,ms,mv,mvi,pl,rms/lsig/APaTxxMwRgIhAKXNlViU3AdSFeWASE3LIMcVhNogkFhP_Zk0Y0kH9gV0AiEApibNtsfPUyA452CrsGJv0PuAi_TqljxZyXW7eIt5iwg%3D/playlist/index.m3u8
+
