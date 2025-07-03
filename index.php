da913dc0264be0efc8e6f7848560b32d0486e930
247tivi <lutn001@gmail.com>
2025-07-03 15:02:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 #EXTM3U
 #EXTINF:-1 ,vtv2
 #EXTVLCOPT:http-user-agent=Dalvik/2.1.0
-https://s2129133.cdn.mytvnet.vn/clrlive_10.m3u8?c=vstv451&q=hd&deviceType=2&userId=20648399&deviceId=28096968&type=tv&p=0&zn=netott&ctl=239.173&hc=ctl21&sl=2&drmsrv=ctl21.mytvnet.vn&naddr=113.171.129.133&pkg=pkg32.hni&profiles=hd,sd,high,medium&&bhd=7500000&bsd=5500000&bhi=3500000&bme=1500000&location=HDG&rbk=17216246120&pnode=172.16.237.68&isp=all&rkey=20648399m99RqJe5
+#https://s2129133.cdn.mytvnet.vn/clrlive_10.m3u8?c=vstv451&q=hd&deviceType=2&userId=20648399&deviceId=28096968&type=tv&p=0&zn=netott&ctl=239.173&hc=ctl21&sl=2&drmsrv=ctl21.mytvnet.vn&naddr=113.171.129.133&pkg=pkg32.hni&profiles=hd,sd,high,medium&&bhd=7500000&bsd=5500000&bhi=3500000&bme=1500000&location=HDG&rbk=17216246120&pnode=172.16.237.68&isp=all&rkey=20648399m99RqJe5
+https://s2129133.cdn.mytvnet.vn/clrlive_10.m3u8
